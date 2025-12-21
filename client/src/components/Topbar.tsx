9eaf23b8a748648e6b219374b331af3e48f74fb6
Karan Singh <devkaran.codes@gmail.com>
2025-12-21 23:34:43 +0530

    react unused

--- client/src/components/Topbar.tsx
+++ client/src/components/Topbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { User, Bell } from "lucide-react";
 import api from "../services/api";
